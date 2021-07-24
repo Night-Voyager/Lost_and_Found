940b1da5e8550d267dc646b8dc8aa021410a1ad8
Night-Voyager <18722007@bjtu.edu.cn>
2021-07-25 03:00:51 +0800

    feat(backend): switch the backend to be a submodule

--- backend/Lost/apps/miniprogramAPI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MiniprogramapiConfig(AppConfig):
-    name = 'apps.miniprogramAPI'
--- backend/Lost/apps/miniprogramAPI/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^api-test/$', views.Test.as_view())
-]
